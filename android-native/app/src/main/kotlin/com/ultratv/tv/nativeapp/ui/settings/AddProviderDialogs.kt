7f594cb8bcd6b050232000925eaa604a8534103d
Mario Elvir <melvir@outlook.com>
2026-07-12 20:28:44 -0700

    Corregir fallo de compilación en AddProviderDialogs.kt

--- android-native/app/src/main/kotlin/com/ultratv/tv/nativeapp/ui/settings/AddProviderDialogs.kt
+++ android-native/app/src/main/kotlin/com/ultratv/tv/nativeapp/ui/settings/AddProviderDialogs.kt
@@ -25,7 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusGroup
+import androidx.compose.foundation.focusGroup
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.foundation.focusable
 import androidx.compose.ui.graphics.Color
